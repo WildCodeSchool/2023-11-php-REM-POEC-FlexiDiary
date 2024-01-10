81cb8a915474b9ac070b0e3e979634d263dd07be
Esoriak <somahaut.dev@gmail.com>
2024-01-10 15:44:42 +0100

    add validate function in form create article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -20,43 +20,28 @@ class ArticleController extends AbstractController
             $extension = pathinfo($_FILES['picture']['name'], PATHINFO_EXTENSION);
             $uploadFile = $uploadDir . uniqid() . '.' . $extension;
             $authorizedExtensions = ['jpg', 'jpeg', 'png', 'webp'];
-            $maxFileSize = 10000000;
-            if ($_FILES['picture']['error'] !== 0) {
-                $errors[] = "Erreur de transfert de fichier. L'article n'a pas été ajouté.";
-            }
             $newArticle = array_map('trim', $_POST);
-            if (strlen($newArticle['Title']) === 0  || strlen($newArticle['Title']) > 255) {
-                $errors[] = "Le titre de l'article doit faire au minimum 2 caractères et maximum 255 caractères";
-            }
-            if (strlen($newArticle['Content']) === 0) {
-                $errors[] = "le contenu de l'article ne peut pas être vide.";
-            }
-            if (!in_array($extension, $authorizedExtensions)) {
-                $errors[] = 'Selectionnez une image de type JPG, JPEG, PNG, OU WEBP.';
-            }
-            if (
-                file_exists($_FILES['picture']['tmp_name']) &&
-                filesize($_FILES['picture']['tmp_name']) > $maxFileSize
-            ) {
-                $errors[] = "Votre image est trop lourde 1Mo max.";
-            }
+
             if (isset($newArticle['visibility'])) {
                 $newArticle['visibility'] = true;
             } else {
                 $newArticle['visibility'] = false;
             }
-            if (!isset($newArticle['tags'])) {
-                $errors[] = " Tu dois ranger ton article dans une catégorie !";
-            }
+            $errors = $this->validate($newArticle, $extension, $authorizedExtensions);
             if (!move_uploaded_file($_FILES['picture']['tmp_name'], $baseDir . $uploadFile)) {
                 $errors[] = $baseDir . $uploadFile;
             }
+            $dataArticleSecure = [];
+            $dataArticleSecure['Title'] = htmlentities($newArticle['Title']);
+            $dataArticleSecure['Content'] = htmlentities($newArticle['Content']);
+            $dataArticleSecure['visibility'] = $newArticle['visibility'];
+            $dataArticleSecure['tags'] = $newArticle['tags'];
             if (empty($errors)) {
                 $imageArticle = $uploadFile;
                 $dateCreation = new DateTimeImmutable('now');
                 $dateFormat = $dateCreation->format("Y-m-d");
                 $article = new ArticleManager();
-                $article->insert($newArticle, $dateFormat, $imageArticle, $idBlog);
+                $article->insert($dataArticleSecure, $dateFormat, $imageArticle, $idBlog);
                 header('Location:/blog/show?idBlog=' . $idBlog);
                 return null;
             }
@@ -68,5 +53,34 @@ class ArticleController extends AbstractController
             'tags' => $tags,
             'idBlog' => $idBlog
         ]);
+    }
+
+
+    private function validate(array $newArticle, string $extension, array $authorizedExtensions)
+    {
+        $maxFileSize = 1000000;
+        $errors = [];
+        if ($_FILES['picture']['error'] !== 0) {
+            $errors[] = "Erreur de transfert de fichier. L'article n'a pas été ajouté.";
+        }
+        if (strlen($newArticle['Title']) === 0  || strlen($newArticle['Title']) > 255) {
+            $errors[] = "Le titre de l'article doit faire au minimum 2 caractères et maximum 255 caractères";
+        }
+        if (strlen($newArticle['Content']) === 0) {
+            $errors[] = "le contenu de l'article ne peut pas être vide.";
+        }
+        if (!in_array($extension, $authorizedExtensions)) {
+            $errors[] = 'Selectionnez une image de type JPG, JPEG, PNG, OU WEBP.';
+        }
+        if (
+            file_exists($_FILES['picture']['tmp_name']) &&
+            filesize($_FILES['picture']['tmp_name']) > $maxFileSize
+        ) {
+            $errors[] = "Votre image est trop lourde 1Mo max.";
+        }
+        if (!isset($newArticle['tags'])) {
+            $errors[] = " Tu dois ranger ton article dans une catégorie !";
+        }
+        return $errors;
     }
 }
